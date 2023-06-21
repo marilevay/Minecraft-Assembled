39b897b02acf7a0530156d38487182db6c8d6cfa
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2023-06-20 21:23:34 -0300

    Update matrixinversor.py

--- matrixinversor.py
+++ matrixinversor.py
@@ -8,7 +8,7 @@ for i in range(n):
     row = [array[i] for i in range(i*n, n*(i+1))]
     matrix.append(row)
 nm = [[0 for i in range(n)] for i in range(n)]
-#saída: única linha com os valores dos pixels rotacionar a imagem 90 graus para a direita
+#saída: única linha com os valores dos pixels após rotacionar a imagem 90 graus para a direita
 for i in range(n):
     for j in range(n):
         nm[i][j] = matrix[n-1-j][i]
