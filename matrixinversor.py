dfbc86c2f4cfeb636ebd2bcd40bad833da5eac23
Arthur Botelho <126793795+arthur9548@users.noreply.github.com>
2023-06-20 21:22:55 -0300

    Add files via upload

---
+++ matrixinversor.py
@@ -0,0 +1,15 @@
+#entrada: tamanho da matriz quadrada de pixels
+n = int(input())
+#entrada: linha única com valores dos pixels separados por vírgulas
+#(assim como gerado por pixel2dotdata.py e provavelmente o conversor de oac)
+array = [int(i) for i in input().split(',')]
+matrix = []
+for i in range(n):
+    row = [array[i] for i in range(i*n, n*(i+1))]
+    matrix.append(row)
+nm = [[0 for i in range(n)] for i in range(n)]
+#saída: única linha com os valores dos pixels rotacionar a imagem 90 graus para a direita
+for i in range(n):
+    for j in range(n):
+        nm[i][j] = matrix[n-1-j][i]
+        print(nm[i][j], end=',')
